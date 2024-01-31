f687b057cec410e84951052304996353b9f118c6
MarciHuMex <151879261+MarciHuMex@users.noreply.github.com>
2024-01-31 21:12:41 +0100

    Passed very firs test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+const repeatString =  function (string, number) {
+    if (number<0) return 'ERROR';
+        let word='';
+        for (i=0; i<number; i++) {
+            word += string;
+        }
+        return word
+    };
+    // Do not edit below this line
+    module.exports = repeatString;
